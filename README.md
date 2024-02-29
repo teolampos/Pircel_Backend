605d2d09de906bb5cef9e0f52ff4e9d79532bec2
Teo <teolampos@gmail.com>
2024-02-29 15:47:00 +0200

    feat: Implement Pircel_App REST API

---
+++ README.md
@@ -0,0 +1,1 @@
+# Pircel_Backend
---
+++ app.js
@@ -0,0 +1,31 @@
+const express = require("express");
+const cors = require("cors");
+const data = require("./data");
+require("dotenv").config();
+
+const PORT = process.env.PORT || 4000;
+const app = express();
+
+app.use(express.json());
+app.use(
+  cors({
+    origin: ["http://localhost:3000"],
+  })
+);
+
+app.get("/houses", async (req, res) => {
+  try {
+    const name = req.query.name;
+    if (name) {
+      const filteredHouses = data.houses.filter((house) =>
+        house.name.includes(name)
+      );
+      return res.status(200).json(filteredHouses);
+    }
+    res.status(200).json(data.houses);
+  } catch (err) {
+    res.status(500).json({ msg: "Internal Server Error" });
+  }
+});
+
+app.listen(PORT, () => console.log(`Listening on port ${PORT}...`));
